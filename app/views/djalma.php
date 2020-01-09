30646a93a99c52100b056ae5aa12134c3c25a1ba
Matheus-Sampaio <matheusballestra@hotmail.com>
2020-01-09 15:07:07 -0300

    adição de artigos

--- app/views/djalma.php
+++ app/views/djalma.php
@@ -22,6 +22,10 @@
                 <ul>
                     <li><a href="#PARTIR... CAMINHAR">PARTIR... CAMINHAR</a></li>
                     <li><a href="#MOÇÃO DE APOIO AOS EXCLUÍDOS">MOÇÃO DE APOIO AOS EXCLUÍDOS</a></li>
+                    <li><a href="#EXPERIÊNCIAS DO NATAL">EXPERIÊNCIAS DO NATAL</a></li>
+                    <li><a href="#SAUDADES PERENES">SAUDADES PERENES</a></li>
+                    <li><a href="#VENCERÁS">VENCERÁS</a></li>
+                    <li><a href="#ÍCONES DA MINHA VIDA">ÍCONES DA MINHA VIDA</a></li>
                 </ul>
         </div>
     </aside>
@@ -53,8 +57,72 @@
                     <p>Reafirmamos a nossa fé num Deus Eterno, justo e sensível ao clamor do povo, que dá forças aos que se mobilizam para a conquista humana da terra, o direito à saúde, educação e moradia.</p>
                     <p>Como igreja cristã, Nazareth continuará a apoiar a luta dos excluídos. Entendemos que a existência dos milhões de excluídos, hoje, no Brasil, revela uma profunda e triste situação anticristã.</p>
                     <p>Acreditamos que o clamor do povo será determinante para o despertamento do poder público para os problemas sociais, como a justa distribuição da terra, da habitação digna, de mais educação e assistência à saúde. É assim que se cumprirá a Justiça Divina.</p>
-                    <p>Desse modo, fazemos a nossa confissão pública, neste sete de setembro.</p>
+                    <p id="EXPERIÊNCIAS DO NATAL">Desse modo, fazemos a nossa confissão pública, neste sete de setembro.</p>
                     <p class="direita">Salvador, Bahia, 7 de setembro de 1995</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">	
+        <div class="row">
+            <div class="col-md-12 col-xs-12">
+                <div class="artigo">
+                    <h2>EXPERIÊNCIAS DO NATAL</h2>
+                    <cite>Pr. Djalma Torres<cite>
+                    <p>Hoje vivi uma experiência inusitada, como pastor. Estava hospitalizado na AMO desde às 7h30 da manhã, quando por volta das 11h, alguém da TV Bahia me telefonou pedindo para gravar uma mensagem de Natal, que concordei. Perguntado sobre onde eu me encontrava, respondi que estava hospitalizado. Houve um espanto muito grande, com a pergunta sobre o que havia acontecido. Respondi que estava tudo bem, estava cuidando de um problema de saúde.</p>
+                    <p>Marcamos a gravação para depois do tratamento, por volta das 13h. Deixei o espaço da sessão do internamento, tentei mudar o perfil de doente sofrido, saindo de uma sessão de quimioterapia, para um pastor com uma rica e entusiasmada experiência a respeito do relato bíblico da encarnação divina, em celebrações sempre repetidas anualmente, mas também sempre emocionantes, do nascimento de Jesus.</p>
+                    <p>E fui para a recepção, local acertado para a entrevista. Depois de instalados os equipamentos e pronta a equipe, me preparei para começar, quando chegou ao local um representante da AMO querendo barrar a entrevista, julgando que se tratava de alguma denúncia, o que me levou a esclarecer que eu era pastor protestante e que se tratava de uma entrevista sobre o Natal. Logo em seguida veio outro representante, que recebeu a mesma explicação de minha parte e a garantia da equipe de TV de que esse era realmente o tema da gravação. Mas, para ter certeza, ele permaneceu no local até o fim da entrevista.</p>
+                    <p>Além dessa situação, pacientes de todo jeito entravam e saiam, me deixando meio desconfortável com o inusitado da experiência, tão diferente de uma das muitas entrevista que tenho dado em locais mais adequado, em celebrações com grupos e em Igrejas, devidamente paramentado e embalado por liturgias bem elaboradas nas muitas comemorações do Natal de Jesus Cristo.</p>
+                    <p id="SAUDADES PERENES">Mas me consolei com o fato de que Jesus nasceu num lugar inóspito, sem conforto e na pobreza da falta de condições apropriadas para o nascimento de um ser humano, não obstante ter Ele vindo ao mundo para anunciar e viver o projeto divino de reconciliação desse mesmo ser humano com o Deus Eterno.</p>
+                    <p class="direita">Salvador, Bahia, 24 de dezembro de 2019</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">	
+        <div class="row">
+            <div class="col-md-12 col-xs-12">
+                <div class="artigo">
+                    <h2>SAUDADES PERENES</h2>
+                    <cite>Pr. Djalma Torres<cite>
+                    <p>Há um mês tenho trabalhado na reorganização da minha biblioteca, que amo. Foi então que descobri uma agenda de 2016, que recebi da Convenção Batista Baiana, oferecida pelo Pr. Edvar Gimenes, com a seguinte dedicatória: Ao Pastor Djalma Torres, desejando-lhe um ótimo 2016. Edvar, SAA, 26.11.2015.</p>
+                    <p>A agenda vinha repleta de textos bíblicos e mensagens diárias, com espaço ainda para as anotações de cada dia. Entreguei-a Vone, que a merecia mais do que eu, com a seguinte dedicatória:</p>
+                    <p>Vone, companheira querida de mais de 45 anos. Há gestos que marcam mais do que discursos e presentes. Achei que esta agenda é mais útil e importante pra você do que pra mim.</p>
+                    <p id="VENCERÁS">Você cultiva valores e tradições batistas cujas fronteiras eu já ultrapassei e nesta agenda há datas, textos bíblicos e mensagens que alimentam o dia a dia da fé cristã. Entrego-a a você com o carinho e a gratidão de um amor que aumenta sempre.</p>
+                    <p class="direita">Salvador, Bahia, 29 de novembro de 2015</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">	
+        <div class="row">
+            <div class="col-md-12 col-xs-12">
+                <div class="artigo">
+                    <h2>VENCERÁS</h2>
+                    <cite>José G. Madeira<cite>
+                    <p>Não desanimes.<br>Persista mais um tanto. <br>Não cultive pessimismo. <br>Centraliza-te no bem a fazer. <br>Esqueça as sugestões do medo destrutivo. <br>Siga adiante, mesmo vazando a sombra dos próprios erros. <br>Avance ainda que seja entre lágrimas. <br>Trabalhe constantemente. <br>Edifique sempre <br>Não consintas que o gelo do desencanto te entorpeça o coração. <br>Não te preserve a dificuldade. <br>Concentra-te de que a vitória espiritual é construção para o dia a dia. <br>Não desista da paciência. <br>Não creias em realização sem esforço. <br>Silêncio para injúria. <br>Olvido para o mal. <br>Perdão às ofensas. <br>Recorda que os agressores são doentes. <br>Não permita que os irmãos desequilibrados te destruam o trabalho ou te apaguem a esperança. <br>Não menosprezes o dever que a consciência te impõe. <br>Se te enganaste em algum trecho do caminho, reajusta a própria visão e procura o rumo certo. <br>Não contes vantagens, nem fracassos. <br>Estuda buscando aprender. <br>Não te soltes contra ninguém. <br>Não dramatizes provocações ou problemas. <br>Conserva o hábito da oração, para que se te faça, luz na vida íntima. <br>Resguarda-te em Deus <br>Persevera no trabalho que Deus te confiou. <br id="ÍCONES DA MINHA VIDA">Ama sempre, fazendo pelos outros o melhor que possas realizar. <br>E assim vencerás.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">	
+        <div class="row">
+            <div class="col-md-12 col-xs-12">
+                <div class="artigo">
+                    <h2>ÍCONES DA MINHA VIDA</h2>
+                    <p>Eu havia colocado como ícones da minha vida cultural, no sentido mais amplo e compreensível do termo, incluindo o conhecimento social, teológico, filosófico, psicanalítico e do mundo, três grandes personagens, incontestavelmente exponenciais: Hans Kung, teólogo suíço, autor de duas obras fascinantes: Ser Cristão (em português) e Existe Dios? (em espanhol), entre muitas outras; Pierre Teilhard de Chardin, Jesuíta, Teólogo, Paleontólogo, escritor, antropólogo, cientista e místico, autor de O Fenômeno Humano, O Meio Divino e diversas outras obras; e Sigmund Freud, o pai da Psicanálise e autor de inúmeras obras sobre o inconsciente humano.</p>
+                    <p>E estava feliz nessa tríplice companhia, quando o meu amigo Raimundo Dantas posta no Facebook, um texto autobiográfico de Bertrand Russel, que me deixou fascinado, porque representa o que sinto, o que venho fazendo e o que estou buscando incessantemente.</p>
+                    <h3>AS TRÊS PAIXÕES</h3>
+                    <cite>Bertrand Russel<cite>
+                    <p>Três paixões, simples, mas esmagadoramente intensas, têm governado minha vida; a ânsia de amor, a busca do conhecimento e uma insuportável piedade pelo sofrimento da humanidade. Essas três paixões, como grandes vendavais, me têm levado de cá para lá, por uma rota cambiante, sobre um profundo oceano de angústia, até à borda mesmo do desespero.</p>
+                    <p>Busquei o amor, primeiro, porque conduz ao êxtase, um êxtase tão grande que, com frequência, teria sacrificado o resto da minha existência por umas horas deste gozo. Busquei-o, em segundo lugar, porque alivia a solidão, essa terrível solidão em que uma consciência trêmula se assoma à beira do mundo para observar o frio e insondável abismo sem vida. Busquei-o, finalmente, porque na união do amor tenho visto, em miniatura mística, a visão antecipada do Céu imaginado por santos e poetas. Isto era o que buscava e, ainda que pudesse parecer demasiado bom para esta vida humana, isto é o que – ao fim –encontrei.</p>
+                    <p>Com igual paixão busquei o conhecimento. Desejei entender o coração dos homens. Desejei saber por que brilham as estrelas. E tratei de apreender o poder pitagórico em virtude do qual o número domina o fluxo. Algo disto consegui, embora não muito.</p>
+                    <p>O amor e o conhecimento, na medida em que ambos eram possíveis, me transportavam ao céu. Porém sempre a piedade me fazia voltar à terra. Ressoa em meu coração o eco dos gritos de dor. Crianças famintas, vítimas torturadas por opressores, anciãos desvalidos, carga odiosa para seus filhos, e todo um mundo de solidão, pobreza e dor convertem em uma burla o que deveria ser a existência humana. Desejo ardentemente aliviar o mal, porém não posso, e eu também sofro.</p>
+                    <p>Esta foi minha vida. Julguei-a digna de se viver, e com gosto voltaria a vivê-la se se me oferecesse a oportunidade.</p>
                 </div>
             </div>
         </div>
@@ -74,6 +142,8 @@
             </div>
         </div>
     </div>
+
+
 </div>
     
 
